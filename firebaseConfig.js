b39bf6f39b6dbd5827ea9f37f38b7a42cbd71762
Nahar Saias <nahar2002@gmail.com>
2025-04-02 20:02:39 -0400

    test no vite

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDJplJXbIhLyXt2mckNBV6V4Xi5FfMF7Ws",
+  authDomain: "ani-mates-7d7f1.firebaseapp.com",
+  projectId: "ani-mates-7d7f1",
+  storageBucket: "ani-mates-7d7f1.firebasestorage.app",
+  messagingSenderId: "660304955101",
+  appId: "1:660304955101:web:15c81c362c972ab2143d34",
+  measurementId: "G-SHCE6J2CE1",
+};
---
+++ signInWithPopup.js
@@ -0,0 +1,23 @@
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  getAuth,
+} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import firebaseConfig from "./firebaseConfig.js";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const PARENT_FRAME = document.location.ancestorOrigins[0];
+const PROVIDER = new GoogleAuthProvider();
+
+function sendResponse(result) {
+  globalThis.parent.self.postMessage(JSON.stringify(result), PARENT_FRAME);
+}
+
+globalThis.addEventListener("message", function ({ data }) {
+  if (data.initAuth) {
+    signInWithPopup(auth, PROVIDER).then(sendResponse).catch(sendResponse);
+  }
+});
